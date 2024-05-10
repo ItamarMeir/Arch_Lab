b60554f080383e7b65f005a1a3be214dfef36789
ItamarMeir <itamarmeir1@gmail.com>
2024-05-10 17:03:39 +0300

    mux

--- try.vhd
+++ try.vhd
@@ -1,3 +1,13 @@
 library ieee
 use ieee.std_logic_1164.all;
 =========================
+entity mux is 
+    port (a,b,c,d,s0,s1 : in std_logic;
+        y : out std_logic; )
+=========================
+architecture Log of mux is begin
+    y <= (a and not s0 and not s1) or
+        (b and not s1 and s0) or
+        (c and s1 and not s0) or
+        (d and s1 and s0);
+    end Log;
